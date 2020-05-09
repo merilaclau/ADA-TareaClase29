88e1c715090572940b0d8eac6ebf360695f49ea2
Meri Laclau <merilaclau@gmail.com>
2020-05-09 05:31:10 -0300

    tarea concatenar javascript

---
+++ file.js
@@ -0,0 +1,11 @@
+//Crear 2 constantes usando prompts.//
+//Con esas dos constantes, crear una tercera constante o variable juntando las dos primeras.
+//El resultado esperado//
+//resultado "palabra1 palabra2"//
+
+const nombre = "Meri";
+const apellido = "Laclau"
+const nombreCompleto = nombre + ' ' + apellido;
+alert (nombreCompleto);
+
+`${nombre} ${apellido}`
